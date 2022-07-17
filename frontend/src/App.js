6627122a7e793bb26ba80514dc5b8d2461ec6139
afridihannan <afridihannan325@gmail.com>
2022-07-18 00:51:34 +0530

    team1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import About from "./Components/About/About";
 import Icrame from "./Components/Icrame/Icrame";
+import Team from "./Components/Ourteam/Team";
 
 function App() {
   return (
     <div className="App">
       <About/>
       <Icrame/>
+      <Team/>
     </div>
   );
 }
