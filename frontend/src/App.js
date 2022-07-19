04bf5607f3bc503830b54949dcc28b0c43bd5e45
Adhiraj <adhirajd007@gmail.com>
2022-07-20 02:06:33 +0530

    added variety podcast
    demo playlist used in the page. have to add playlist from mes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import About from "./Components/About/About";
 import Icrame from "./Components/Icrame/Icrame";
+import Variety from "./Components/Variety/Variety";
 
 function App() {
   return (
     <div className="App">
       <About/>
       <Icrame/>
+      <Variety/>
     </div>
   );
 }
--- frontend/src/Components/Events/Event.jsx
+++ frontend/src/Components/Events/Event.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Event() {
   return (
-    <div>
+    <div className="events">
         
     </div>
   )
